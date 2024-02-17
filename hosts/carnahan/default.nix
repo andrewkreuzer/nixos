cb26a9b3bf0769967bcce524bf8994936aaf8650
andrewkreuzer <me@andrewkreuzer.com>
2024-02-16 21:46:13 -0500

    consolidate nix settings

--- hosts/carnahan/default.nix
+++ hosts/carnahan/default.nix
@@ -108,18 +108,6 @@ with lib;
     '';
   };
 
-  nix = {
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
-    settings = {
-      substituters = [ "https://hyprland.cachix.org" "https://cuda-maintainers.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
-    };
-  };
-
   fileSystems = let
     mkZfsMount = dataset: {
       device = "${dataset}";
--- system/default.nix
+++ system/default.nix
@@ -37,11 +37,22 @@ with lib;
     sudo.enable = mkForce false;
   };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.extraOptions = ''
-    keep-outputs = true
-    keep-derivations = true
-  '';
+  nix = {
+    settings.experimental-features = [ "nix-command" "flakes" ];
+    extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+      '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
+    };
+    settings = {
+      substituters = [ "https://hyprland.cachix.org" "https://cuda-maintainers.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
+    };
+  };
 
   networking.wg-quick.interfaces = {
     wg0 = {
