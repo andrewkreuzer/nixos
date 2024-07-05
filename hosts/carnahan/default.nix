16d053e393bd12e00e175286d324cc83c288ce79
andrewkreuzer <me@andrewkreuzer.com>
2024-07-05 08:38:10 -0400

    logind and power profiles

--- hosts/carnahan/default.nix
+++ hosts/carnahan/default.nix
@@ -14,6 +14,11 @@ with lib;
   };
 
   services = {
+    logind = {
+      powerKey = "poweroff";
+      lidSwitch="suspend";
+      lidSwitchExternalPower= "suspend";
+    };
     fwupd.enable = true;
     blueman.enable = true;
     hardware.bolt.enable = true;
@@ -33,6 +38,12 @@ with lib;
       enable = true;
       settings = {
         SOUND_POWER_SAVE_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+        CPU_MAX_PERF_ON_BAT = 30;
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       };
     };
     pipewire = {
